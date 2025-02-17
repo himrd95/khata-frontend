4d2aa7516b4b21490ffdb5e9a1bbd15b5e228f4f
Himanshu Dwivedi <73213831+himrd95@users.noreply.github.com>
2025-02-18 01:11:58 +0530

    updated login page

--- src/Components/Auth/Registration.jsx
+++ src/Components/Auth/Registration.jsx
@@ -1,141 +1,141 @@
-import { Button } from '@mui/material';
-import axios from 'axios';
-import React, { useContext, useState } from 'react';
-import { BASE_URL } from '../../constants';
-import { provider } from '../../Context/ContextPovider';
-import SimpleDialog from '../Modal';
-import { SuccessPopupContent } from '../SuccessPopupContent';
-import LoginContent from './LoginContent';
-import './Registration.css';
-import RegistrationContent from './RegistrationContent';
-import dotenv from 'dotenv';
+import { Button } from "@mui/material";
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { BASE_URL } from "../../constants";
+import { provider } from "../../Context/ContextPovider";
+import SimpleDialog from "../Modal";
+import { SuccessPopupContent } from "../SuccessPopupContent";
+import LoginContent from "./LoginContent";
+import "./Registration.css";
+import RegistrationContent from "./RegistrationContent";
+import dotenv from "dotenv";
 
 const Registration = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	const [content, setContent] = useState('');
-	const { loginPopup, setAdminPannel, setIsLoading, setError } =
-		useContext(provider);
-	dotenv.config();
+    const [isOpen, setIsOpen] = useState(false);
+    const [content, setContent] = useState("");
+    const { loginPopup, setAdminPannel, setIsLoading, setError } =
+        useContext(provider);
+    dotenv.config();
 
-	const handleClose = () => {
-		setIsOpen(false);
-	};
+    const handleClose = () => {
+        setIsOpen(false);
+    };
 
-	const successPopup = () => {
-		setContent(
-			<SuccessPopupContent handleLoginModal={handleLoginModal} />,
-		);
-	};
-	const handleAddUser = (payload) => {
-		const { name, age, email, password, profile } = payload;
-		const formData = new FormData();
-		formData.append('name', name);
-		formData.append('age', age);
-		formData.append('email', email);
-		formData.append('password', password);
-		formData.append('profile', profile);
-		setError({ message: '', open: false });
-		setIsLoading(true);
-		const url = `${BASE_URL}/register/`;
-		axios
-			.post(url, formData)
-			.then((res) => {
-				setIsLoading(false);
-				if (res.data.token) {
-					console.log(res.data);
-					successPopup();
-				} else
-					setError({
-						message: 'User has been already registered!',
-						open: true,
-					});
-			})
-			.catch((e) =>
-				setError({
-					message: 'Something went wrong please try again later!',
-					open: true,
-				}),
-			)
-			.finally(() => setIsLoading(false));
-	};
-	const handleRegistrationModal = () => {
-		setError({ message: '', open: false });
-		setIsOpen(true);
-		setContent(
-			<RegistrationContent
-				handleClose={handleClose}
-				handleAddUser={handleAddUser}
-			/>,
-		);
-	};
+    const successPopup = () => {
+        setContent(<SuccessPopupContent handleLoginModal={handleLoginModal} />);
+    };
+    const handleAddUser = (payload) => {
+        const { name, age, email, password, profile } = payload;
+        const formData = new FormData();
+        formData.append("name", name);
+        formData.append("age", age);
+        formData.append("email", email);
+        formData.append("password", password);
+        formData.append("profile", profile);
+        setError({ message: "", open: false });
+        setIsLoading(true);
+        const url = `${BASE_URL}/register/`;
+        axios
+            .post(url, formData)
+            .then((res) => {
+                setIsLoading(false);
+                if (res.data.token) {
+                    console.log(res.data);
+                    successPopup();
+                } else
+                    setError({
+                        message: "User has been already registered!",
+                        open: true,
+                    });
+            })
+            .catch((e) =>
+                setError({
+                    message: "Something went wrong please try again later!",
+                    open: true,
+                })
+            )
+            .finally(() => setIsLoading(false));
+    };
+    const handleRegistrationModal = () => {
+        setError({ message: "", open: false });
+        setIsOpen(true);
+        setContent(
+            <RegistrationContent
+                handleClose={handleClose}
+                handleAddUser={handleAddUser}
+            />
+        );
+    };
 
-	const handleLogin = (payload) => {
-		console.log(BASE_URL, '+++++');
-		setError({ message: '', open: false });
-		setIsLoading(true);
-		const url = `${BASE_URL}/login/`;
-		axios
-			.post(url, { ...payload })
-			.then((res) => {
-				setIsLoading(false);
-				if (res.data.token) {
-					setAdminPannel(res.data);
-					handleClose();
-				} else
-					setError({ message: 'Wrong credentials!', open: true });
-			})
-			.catch((e) =>
-				setError({ message: 'Wrong credentials!', open: true }),
-			)
-			.finally(() => setIsLoading(false));
-	};
-	const handleLoginModal = () => {
-		setError({ message: '', open: false });
-		setIsOpen(true);
-		setContent(
-			<LoginContent
-				handleClose={handleClose}
-				handleLogin={handleLogin}
-			/>,
-		);
-	};
-	React.useEffect(() => {
-		if (loginPopup) {
-			handleLoginModal();
-		}
-	}, [loginPopup]);
-	return (
-		<div className="messages">
-			<div>
-				<div className="unused"></div>
-				<h1>Tired of managing your daily expenses?</h1>
-				<h3>Here's the solution. So let's</h3>
-				<Button
-					onClick={() => handleRegistrationModal()}
-					sx={{ padding: '10px 25px' }}
-					variant="contained"
-					color="primary"
-				>
-					Get Started
-				</Button>
-				<p>
-					Already registered?{' '}
-					<Button
-						onClick={() => handleLoginModal()}
-						sx={{ color: '#2bc3ff' }}
-					>
-						Login
-					</Button>
-				</p>
-			</div>
+    const handleLogin = (payload) => {
+        console.log(BASE_URL, "+++++");
+        setError({ message: "", open: false });
+        setIsLoading(true);
+        const url = `${BASE_URL}/login/`;
+        axios
+            .post(url, { ...payload })
+            .then((res) => {
+                setIsLoading(false);
+                if (res.data.token) {
+                    setAdminPannel(res.data);
+                    handleClose();
+                } else setError({ message: "Wrong credentials!", open: true });
+            })
+            .catch((e) =>
+                setError({ message: "Wrong credentials!", open: true })
+            )
+            .finally(() => setIsLoading(false));
+    };
+    const handleLoginModal = () => {
+        setError({ message: "", open: false });
+        setIsOpen(true);
+        setContent(
+            <LoginContent handleClose={handleClose} handleLogin={handleLogin} />
+        );
+    };
+    React.useEffect(() => {
+        if (loginPopup) {
+            handleLoginModal();
+        }
+    }, [loginPopup]);
+    return (
+        <div
+            className="messages"
+            style={{ backgroundImage: "url(/money_exchange_.png)" }}
+        >
+            <div>
+                <div className="unused"></div>
+                <h1>Tired of managing your daily expenses?</h1>
 
-			<SimpleDialog
-				isOpen={isOpen}
-				handleClose={handleClose}
-				content={content}
-			/>
-		</div>
-	);
+                <div className="bottom_section">
+                    <h3>Here's the solution. So let's</h3>
+                    <Button
+                        onClick={() => handleRegistrationModal()}
+                        sx={{ padding: "10px 25px" }}
+                        variant="contained"
+                        color="primary"
+                    >
+                        Get Started
+                    </Button>
+                    <p>
+                        Already registered?
+                        <Button
+                            onClick={() => handleLoginModal()}
+                            sx={{ color: "#2bc3ff" }}
+                        >
+                            Login
+                        </Button>
+                    </p>
+                </div>
+            </div>
+
+            <SimpleDialog
+                isOpen={isOpen}
+                handleClose={handleClose}
+                content={content}
+            />
+        </div>
+    );
 };
 
 export default Registration;
